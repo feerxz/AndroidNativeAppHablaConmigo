dfdcfa8da980e979b15ded606752b1b044fcf360
feerxz <Fernandojacinto0931433114@gmail.com>
2024-07-21 22:36:29 -0500

    final demo commit

--- app/src/main/java/com/example/hablaconmigo/ui/shortcuts/ShortCutFragment.java
+++ app/src/main/java/com/example/hablaconmigo/ui/shortcuts/ShortCutFragment.java
@@ -1,24 +1,22 @@
 package com.example.hablaconmigo.ui.shortcuts;
 
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
+import android.widget.TextView;
 
+import com.example.hablaconmigo.R;
 import com.example.hablaconmigo.databinding.FragmentShortCutBinding;
 
-import java.util.ArrayList;
 
 public class ShortCutFragment extends Fragment {
     private FragmentShortCutBinding binding;
-    private AtajosAdapter adapter;
-    private ShortCutViewModel shortCutViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,38 +24,15 @@ public class ShortCutFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        shortCutViewModel = new ViewModelProvider(this).get(ShortCutViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ShortCutViewModel shortCutViewModel = new ViewModelProvider(this).get(ShortCutViewModel.class);
         binding = FragmentShortCutBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // Configurar RecyclerView
-        adapter = new AtajosAdapter(new ArrayList<>());
-        RecyclerView recyclerView = binding.recyclerShortcut;
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
-        recyclerView.setAdapter(adapter);
-
-        // Observar cambios en los datos del ViewModel
-        shortCutViewModel.getCarpetasLiveData().observe(getViewLifecycleOwner(), listaCarpetas -> {
-            adapter.setCarpetas(listaCarpetas);
-            adapter.notifyDataSetChanged();
-        });
-
-        // Configurar SearchView
-        SearchView searchView = binding.searchView;
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                // Filtrar datos (pendiente de implementación)
-                return false;
-            }
-        });
-
+        final TextView textView = binding.textShortcut;
+        shortCutViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/hablaconmigo/ui/shortcuts/ShortCutViewModel.java
+++ app/src/main/java/com/example/hablaconmigo/ui/shortcuts/ShortCutViewModel.java
@@ -1,41 +1,17 @@
 package com.example.hablaconmigo.ui.shortcuts;
-
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.hablaconmigo.R;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class ShortCutViewModel extends ViewModel {
 
-    private final MutableLiveData<List<AtajosAdapter.Carpeta>> carpetasLiveData;
+    private final MutableLiveData<String> mText;
 
     public ShortCutViewModel() {
-        carpetasLiveData = new MutableLiveData<>();
-        cargarDatosIniciales();
+        mText = new MutableLiveData<>();
+        mText.setValue("This is shortcuts fragment");
     }
 
-    public MutableLiveData<List<AtajosAdapter.Carpeta>> getCarpetasLiveData() {
-        return carpetasLiveData;
-    }
-
-    private void cargarDatosIniciales() {
-        List<AtajosAdapter.Carpeta> listaCarpetas = new ArrayList<>();
-        listaCarpetas.add(new AtajosAdapter.Carpeta("Comidas", R.drawable.ic_comida));
-        listaCarpetas.add(new AtajosAdapter.Carpeta("Ubicaciones", R.drawable.ic_ubicaciones));
-        listaCarpetas.add(new AtajosAdapter.Carpeta("Acciones", R.drawable.ic_acciones));
-        listaCarpetas.add(new AtajosAdapter.Carpeta("Estados", R.drawable.ic_estados));
-        listaCarpetas.add(new AtajosAdapter.Carpeta("Mis Datos", R.drawable.ic_mis_datos));
-        carpetasLiveData.setValue(listaCarpetas);
-    }
-
-    public void agregarCarpeta(AtajosAdapter.Carpeta carpeta) {
-        List<AtajosAdapter.Carpeta> listaActual = carpetasLiveData.getValue();
-        if (listaActual != null) {
-            listaActual.add(carpeta);
-            carpetasLiveData.setValue(listaActual);
-        }
+    public MutableLiveData<String> getText() {
+        return mText;
     }
 }
